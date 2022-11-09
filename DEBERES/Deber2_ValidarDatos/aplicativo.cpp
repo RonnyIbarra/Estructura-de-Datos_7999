9377eb20a10bad63a963c0ba9b574421e518287f
RonnyIbarra <rjibarra1@espe.edu.ec>
2022-11-08 23:33:58 -0500

    Deber5

---
+++ DEBERES/Deber2_ValidarDatos/aplicativo.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************
+ * UFA-ESPE
+ * Module:  Datos.cpp
+ * Author:  Ronny Ibarra - Jose Imbaquinga
+ * Modified: domingo, 30 de octubre de 2022 22:49:22
+ * Purpose: Implementation of the class Datos
+ * Level: level three
+ * NRC: 7999
+ ***********************************************************************/
+#include <iostream>
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "Datos.h"
+#include "Datos.cpp"
+using namespace std;
+
+int main(int argc, char** argv) {
+  float Flotantes;
+  double Doble;
+  char Cadena;
+  Doble=*ingresar_double("Ingrese el doble");
+  Flotantes=*ingresar_float("\nIngrese el flotante");
+  Cadena=*ingreso_char("\nIngrese la cadena\n");
+}
