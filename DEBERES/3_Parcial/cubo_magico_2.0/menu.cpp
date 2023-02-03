8e7103f02d44836f309a5ecea7a7f0b1056e58d0
Jose Ricardo Imbaquinga Guaña <99418968+jrimbaquingaguana@users.noreply.github.com>
2023-02-03 01:53:11 -0500

    Cubo_Magico

--- DEBERES/3_Parcial/cubo_magico_2.0/menu.cpp
+++ DEBERES/3_Parcial/cubo_magico_2.0/menu.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
  * UFA - ESPE
  * Students:  Ronny Ibarra - José Imbaquinga
- * Module:  Funciones.cpp
+ * Module:  menu.cpp
  * Created: jueves, 02 de febrero de 2023 19:00:40
  * Modified: jueves, 03 de febrero de 2023 2:02:48
  * Problem: Cúbo Mágico
